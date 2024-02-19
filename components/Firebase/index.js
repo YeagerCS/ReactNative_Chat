170d79f63052604778dd9e5fcb416c36c0d2f334
Lejs <96805967+YeagerCS@users.noreply.github.com>
2024-02-19 09:01:56 +0100

    Update index.js

--- components/Firebase/index.js
+++ components/Firebase/index.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore"
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAksjtjdf4qEoI9fhHsl9EqNfK4ZxQ9VHk",
-  authDomain: "blazorchat-6facb.firebaseapp.com",
-  databaseURL: "https://blazorchat-6facb-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "blazorchat-6facb",
-  storageBucket: "blazorchat-6facb.appspot.com",
-  messagingSenderId: "827600669803",
-  appId: "1:827600669803:web:902d2203ad8de7387a752e"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
